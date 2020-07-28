c26d13a750f7c7c4bea35452a4f199b1cba6763b
CezaryJanicki <cezary.janicki@gmail.com>
2020-07-28 22:35:47 +0200

    third commit

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
     public int add(int a, int b) throws IOException {
         return a + b;
     }
-    public int subtract(int a, intgit  b) throws IOException {
+    public int subtract(int a, int b) throws IOException {
         return a - b;
     }
 
